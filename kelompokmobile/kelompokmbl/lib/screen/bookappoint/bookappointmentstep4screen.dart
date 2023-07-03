b50e809b219f3302ff9684b28a5a91fa9cd46d69
Jessica Hutagalung <jessicauly01@gmail.com>
2023-07-04 04:26:23 +0700

    update step 4

--- kelompokmobile/kelompokmbl/lib/screen/bookappoint/bookappointmentstep4screen.dart
+++ kelompokmobile/kelompokmbl/lib/screen/bookappoint/bookappointmentstep4screen.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kelompokmbl/const.dart';
+import 'package:kelompokmbl/screen/bookappoint/bookappointmentstep5screen.dart';
 import 'package:kelompokmbl/screen/promoscreen.dart';
+import 'package:provider/provider.dart';
 
 class BookAppointmentStep4Screen extends StatefulWidget {
   const BookAppointmentStep4Screen({super.key});
 
   @override
-  State<BookAppointmentStep4Screen> createState() =>
-      _BookAppointmentStep4ScreenState();
+  State<BookAppointmentStep4Screen> createState() => _BookAppointmentStep4ScreenState();
 }
 
-class _BookAppointmentStep4ScreenState
-    extends State<BookAppointmentStep4Screen> {
+enum PaymentMethod { card, cod, ovo, dana, spay, gopay }
+
+class _BookAppointmentStep4ScreenState extends State<BookAppointmentStep4Screen> {
+  PaymentMethod? _paymentItem = PaymentMethod.card;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -71,8 +75,109 @@ class _BookAppointmentStep4ScreenState
                               decoration: TextDecoration.underline),
                         ))),
               ),
-              Center(child: Text('Choose Your Payment Method', style: GoogleFonts.lexendDeca(fontSize: 20, color: Color(0xFF676767)))),
-              
+              Center(
+                  child: Text('Choose Your Payment Method',
+                      style: GoogleFonts.lexendDeca(
+                          fontSize: 20, color: Color(0xFF676767)))),
+              Padding(
+                padding: const EdgeInsets.only(top: 30.0, left: 40.0),
+                child: Column(
+                  children: [
+                    RadioListTile<PaymentMethod>(
+                      value: PaymentMethod.card,
+                      groupValue: _paymentItem,
+                      onChanged: (PaymentMethod? value) {
+                        setState(() {
+                          _paymentItem = value;
+                        });
+                      },
+                      title: Text('Credit Card/Debit Card',
+                          style: GoogleFonts.lexendDeca(
+                              fontSize: 12, color: Colors.black)),
+                    ),
+                    RadioListTile<PaymentMethod>(
+                      value: PaymentMethod.cod,
+                      groupValue: _paymentItem,
+                      onChanged: (PaymentMethod? value) {
+                        setState(() {
+                          _paymentItem = value;
+                        });
+                      },
+                      title: Text('Cash On Delivey',
+                          style: GoogleFonts.lexendDeca(
+                              fontSize: 12, color: Colors.black)),
+                    ),
+                    RadioListTile<PaymentMethod>(
+                      value: PaymentMethod.ovo,
+                      groupValue: _paymentItem,
+                      onChanged: (PaymentMethod? value) {
+                        setState(() {
+                          _paymentItem = value;
+                        });
+                      },
+                      title: Text('OVO',
+                          style: GoogleFonts.lexendDeca(
+                              fontSize: 12, color: Colors.black)),
+                    ),
+                    RadioListTile<PaymentMethod>(
+                      value: PaymentMethod.dana,
+                      groupValue: _paymentItem,
+                      onChanged: (PaymentMethod? value) {
+                        setState(() {
+                          _paymentItem = value;
+                        });
+                      },
+                      title: Text('DANA',
+                          style: GoogleFonts.lexendDeca(
+                              fontSize: 12, color: Colors.black)),
+                    ),
+                    RadioListTile<PaymentMethod>(
+                      value: PaymentMethod.spay,
+                      groupValue: _paymentItem,
+                      onChanged: (PaymentMethod? value) {
+                        setState(() {
+                          _paymentItem = value;
+                        });
+                      },
+                      title: Text('ShopeePay',
+                          style: GoogleFonts.lexendDeca(
+                              fontSize: 12, color: Colors.black)),
+                    ),
+                    RadioListTile<PaymentMethod>(
+                      value: PaymentMethod.gopay,
+                      groupValue: _paymentItem,
+                      onChanged: (PaymentMethod? value) {
+                        setState(() {
+                          _paymentItem = value;
+                        });
+                      },
+                      title: Text('GoPay',
+                          style: GoogleFonts.lexendDeca(
+                              fontSize: 12, color: Colors.black)),
+                    ),
+                  ],
+                ),
+              ),
+              Center(
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 40),
+                      child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFFB3F7F9),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(100)),
+                              padding: const EdgeInsets.symmetric(horizontal: 150),
+                              elevation: 0),
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => const BookAppointmentStep5Screen()));
+                          },
+                          child: Text(
+                            'Pay',
+                            style: GoogleFonts.lexendDeca(
+                                fontSize: 14, fontWeight: FontWeight.w700,color: Color(0xFF454545)),
+                          ))),
+                )
             ],
           ),
         ),
