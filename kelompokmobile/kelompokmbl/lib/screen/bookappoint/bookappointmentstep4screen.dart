aa8e36ab79a6c4242d55d70838de55b55d9ef335
Jessica Hutagalung <jessicauly01@gmail.com>
2023-07-03 23:26:19 +0700

    step 4

--- kelompokmobile/kelompokmbl/lib/screen/bookappoint/bookappointmentstep4screen.dart
+++ kelompokmobile/kelompokmbl/lib/screen/bookappoint/bookappointmentstep4screen.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kelompokmbl/const.dart';
+import 'package:kelompokmbl/screen/promoscreen.dart';
 
 class BookAppointmentStep4Screen extends StatefulWidget {
   const BookAppointmentStep4Screen({super.key});
 
   @override
-  State<BookAppointmentStep4Screen> createState() => _BookAppointmentStep4ScreenState();
+  State<BookAppointmentStep4Screen> createState() =>
+      _BookAppointmentStep4ScreenState();
 }
 
-class _BookAppointmentStep4ScreenState extends State<BookAppointmentStep4Screen> {
+class _BookAppointmentStep4ScreenState
+    extends State<BookAppointmentStep4Screen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +28,52 @@ class _BookAppointmentStep4ScreenState extends State<BookAppointmentStep4Screen>
         centerTitle: true,
         elevation: 0,
         backgroundColor: kPrimaryColor,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          width: double.infinity,
+          height: 800,
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [Color.fromARGB(225, 239, 254, 254), Colors.white])),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 70, bottom: 10),
+                child: Center(
+                    child: Text('Total Cost',
+                        style: GoogleFonts.lexendDeca(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black))),
+              ),
+              Center(
+                  child: Text('IDR 0',
+                      style: GoogleFonts.libreBaskerville(
+                          fontSize: 50, color: const Color(0xFF00DEE4)))),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20.0),
+                child: Center(
+                    child: InkWell(
+                        onTap: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const PromoScreen()));
+                        },
+                        child: Text(
+                          'Voucher in here!',
+                          style: GoogleFonts.notoSerif(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
+                              color: const Color(0xFFEB1F1F),
+                              decoration: TextDecoration.underline),
+                        ))),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
